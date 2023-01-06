9d1023163fe2f71c5905c8fd65ce55e562e58b5b
luke3 <luke3@ualberta.ca>
2023-01-06 13:33:48 -0700

    Add reset and size commands

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import hex_game
 from hex_game import BLACK, WHITE
 from mcts import Mcts
 
+size = 3
+
 def command_loop(game):
     command = None
     while command != "exit" and command != "quit":
@@ -12,23 +14,32 @@ def command_loop(game):
             continue
 
         args = command.split()
-        if args[0] == "x":
-            game.play_move([int(args[1]), int(args[2])], BLACK)
-        elif args[0] == "o":
-            game.play_move([int(args[1]), int(args[2])], WHITE)
-        elif args[0] == "show":
-            print(str(game))
-        elif args[0] == "mcts":
-            if args[1] == "x":
-                mcts = Mcts(game, BLACK)
-                move = mcts.monte_carlo_tree_search()
-                game.play_move(move, BLACK)
-            elif args[1] == "o":
-                mcts = Mcts(game, WHITE)
-                move = mcts.monte_carlo_tree_search()
-                game.play_move(move, WHITE)
+        try:
+            if args[0] == "x":
+                game.play_move([int(args[1]), int(args[2])], BLACK)
+            elif args[0] == "o":
+                game.play_move([int(args[1]), int(args[2])], WHITE)
+            elif args[0] == "show":
+                print(str(game))
+            elif args[0] == "size":
+                global size
+                size = int(args[1])
+                game = hex_game.Hex(size)
+            elif args[0] == "reset":
+                game = hex_game.Hex(size)
+            elif args[0] == "mcts":
+                if args[1] == "x":
+                    mcts = Mcts(game, BLACK)
+                    move = mcts.monte_carlo_tree_search()
+                    game.play_move(move, BLACK)
+                elif args[1] == "o":
+                    mcts = Mcts(game, WHITE)
+                    move = mcts.monte_carlo_tree_search()
+                    game.play_move(move, WHITE)
+        except IndexError:
+            continue
 
 
 if __name__=="__main__":
-    game = hex_game.Hex(5)
+    game = hex_game.Hex(size)
     command_loop(game)
