3bbe9749eb88e0b7d86fb2462128c2da54d397cb
luke3 <luke3@ualberta.ca>
2023-03-23 08:57:38 -0600

    Improve boad representation comment

--- hex_game1.py
+++ hex_game1.py
@@ -5,7 +5,7 @@
 # Board Representation:
 # 3 1 1 1 3    3 1 1 1 3
 # 2 0 0 0 2     2 0 0 0 2
-# 2 0 0 0 2 ->   2 0 0 0 2
+# 2 0 0 0 2 ==   2 0 0 0 2
 # 2 0 0 0 2       2 0 0 0 2
 # 3 1 1 1 3        3 1 1 1 3
 # Where BORDER = 3, BLACK = 1, WHITE = 2, BLANK = 0
